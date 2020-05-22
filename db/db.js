18046782c48823f6cb978cd32df46d4bb5f1c0eb
Nicoline Onchoy Jensen <nicolineonchoy@Nicolines-MacBook-Pro-2.local>
2020-05-22 14:13:23 +0200

    DB

--- db/db.js
+++ db/db.js
@@ -1,4 +1,16 @@
 const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
+const Identity = JSON.parse(
+  fs.readFileSync('../secrets/SECRET.json', 'utf8')
+);
+
+// pool connection?
+const pool = sqlite3.createPool({
+  host: 'itwot00.cs.au.dk',
+  user: Identity.user,
+  password: Identity.password,
+  database: 'VM07'
+});
 
 // open an in-memory database
 let db = new sqlite3.Database(':memory:', (err) => {
@@ -6,6 +18,19 @@ let db = new sqlite3.Database(':memory:', (err) => {
     return console.error(err.message);
   }
   console.log('Connected to the in-memory SQlite database.');
+});
+
+/* Make connection to pool and create the one table? */
+pool.getConnection((err, connection) => {
+  if (err) throw err;
+  connection.query(
+    `CREATE TABLE IF NOT EXISTS insole
+            (timestamp BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, temperature INT, humidity INT)`,
+    err => {
+      if (err) throw err;
+    }
+  );
+  connection.release();
 });
 
 // close the database connection
