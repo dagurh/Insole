ee63274833e160f07b18ae62e9438759784a75aa
Nicoline Onchoy Jensen <nicolineonchoy@Nicolines-MacBook-Pro-2.local>
2020-05-22 12:52:49 +0200

    db virker

--- db/db.js
+++ db/db.js
@@ -1,22 +1,3 @@
-/*nnconst sqlite3 = require('sqlite3').verbose();
-//Skaber et database objekt.
-let db = new sqlite3.Database(':insole:', (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Connected to the insole database.');
-});
-
-db.close((err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Close the database connection.');
-});
-*/
-
-
-
 const sqlite3 = require('sqlite3').verbose();
 
 // open an in-memory database
