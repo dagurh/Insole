a12fb056920c8bfb5350a263fb3e58e54b2015f7
Nicoline Onchoy Jensen <nicolineonchoy@Nicolines-MacBook-Pro-2.local>
2020-05-27 10:14:10 +0200

    db insert function

--- db/db.js
+++ db/db.js
@@ -13,6 +13,14 @@ db.serialize(function() {
   (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, timestamp BIGINT, standing_or_sitting INTEGER)`);
 });
 
+db.run(`INSERT INTO measurements(timestamp, standing_or_sitting) VALUES(?, ?)`, ['C'], function(err) {
+  if (err) {
+    return console.log(err.message);
+  }
+  // get the last insert id
+  console.log(`A row has been inserted with rowid ${this.lastID}`);
+});
+
 db.close((err) => {
   if (err) {
     console.error(err.message);
