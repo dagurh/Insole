144306bd94639d9afcf6f4e2f8a5c01c242b2f2e
William Sonne Ebbesen <1538-au603427@users.noreply.gitlab.au.dk>
2020-06-04 13:26:01 +0200

    Pictures

--- Frontpage/script.js
+++ Frontpage/script.js
@@ -9,25 +9,36 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
   output.innerHTML = this.value;
   console.log(output);
-  if(output.innerHTML < 25) {
+  if(output.innerHTML < 10) {
+    $(".0").show();
+    $(".1").hide();
+    $(".2").hide();
+    $(".3").hide();
+    $(".4").hide();
+  }
+  if(output.innerHTML >= 10 && output.innerHTML < 36 ) {
+    $(".0").hide();
     $(".1").show();
     $(".2").hide();
     $(".3").hide();
     $(".4").hide();
   }
-  if(output.innerHTML >= 25 && output.innerHTML < 50){
+  if(output.innerHTML >= 36 && output.innerHTML < 63){
+    $(".0").hide();
     $(".1").hide();
     $(".2").show();
     $(".3").hide();
     $(".4").hide();
   }
-  if(output.innerHTML >= 50 && output.innerHTML < 75) {
+  if(output.innerHTML >= 63 && output.innerHTML < 90) {
+    $(".0").hide();
     $(".1").hide();
     $(".2").hide();
     $(".3").show();
     $(".4").hide();
   }
-  if(output.innerHTML >= 75) {
+  if(output.innerHTML >= 90) {
+    $(".0").hide();
     $(".1").hide();
     $(".2").hide();
     $(".3").hide();
